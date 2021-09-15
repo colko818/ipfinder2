2913dfef576adc03581004793f2a14c817369077
Gaella Collins-Sibley <gaella@collins-sibley.com>
2021-09-15 19:43:49 -0400

    refactor ping & add dns command

--- worker/worker.py
+++ worker/worker.py
@@ -5,25 +5,60 @@ import sys
 
 from sysconfig import get_platform
 
+from dns import reversename, resolver
 from flask import Flask, abort
+from ping3 import ping
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-tasks = ['ping',]
+tasks = ['ping', "geoip", "dns"]
+
+
+def DoDNS(address):
+    retval = 'DNS:\n'
+    parts = address.split('.')
+
+    # Rough AP parsing
+    if len(parts) == 4:
+        addr = reversename.from_address(address)
+        return retval + str(resolver.resolve(addr, "PTR")[0])
+
+    try:
+        a_records = resolver.resolve(address, 'A')
+        for a in a_records:
+            retval = retval + "A: {}\n".format(str(a))
+    except Exception:
+        retval = retval + "No A records\n"
+
+    try:
+        mx_records = resolver.resolve(address, 'MX')
+        for mx in mx_records:
+            retval = retval + "MX: {}\n".format(str(mx))
+    except Exception:
+        retval = retval + "No MX records\n"
+
+    try:
+        cname_records = resolver.resolve(address, 'CNAME')
+        for cname in cname_records:
+            retval = retval + "CNAME: {}\n".format(str(cname))
+    except Exception:
+        retval+= "No CNAME records\n"
+
+    return retval
+
+
+def DoGeoIP(address):
+    return ""
 
 
 def DoPing(address):
-    cnt = '-n'
-    if get_platform().startswith('win'):
-        cnt = '-n'
-    else:
-        cnt = '-c'
+    delay_s = ping(address, timeout=2)
 
-    cmd = ['ping', cnt, '1', '-w', '500', str(address)]
-    print(cmd)
+    if delay_s is not None:
+        return "Ping:\nHost is alive! {0:.3g}s delay".format(delay_s)
 
-    return subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+    return "Ping:\nHost is dead/timedout :("
 
 
 @app.route('/<addr>/<task>')
@@ -32,10 +67,11 @@ def HandleTask(addr, task):
         abort(400)
 
     if task == 'ping':
-        ret = DoPing(addr)
-
-
-        return ret
+        return DoPing(addr)
+    if task == 'geoip':
+        return DoGeoIP(addr)
+    if task == 'dns':
+        return DoDNS(addr)
 
 
 def start_worker():
