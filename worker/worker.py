f45469c81eb1201718dac336e4e53b0d1037eea1
Gaella Collins-Sibley <gaella@collins-sibley.com>
2021-09-16 12:58:43 -0400

    jsonify worker

--- worker/worker.py
+++ worker/worker.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 import argparse
-import subprocess
 import sys
 
-from sysconfig import get_platform
-
+from dns import reversename, resolver
+from flask import Flask, abort, jsonify
+from ping3 import ping
 from werkzeug.sansio.response import Response
 
-from dns import reversename, resolver
-from flask import Flask, abort
-from ping3 import ping
-
-import requests
 import json
-
+import requests
 import whoisit
 
 app = Flask(__name__)
@@ -25,69 +20,97 @@ tasks = ['ping', "geoip", "dns", "rdap"]
 def DoPing(address):
     delay_s = ping(address, timeout=2)
 
-    if delay_s is not None:
-        return "Ping:\nHost is alive! {0:.3g}s delay".format(delay_s)
+    retval = {
+        'Task': 'Ping',
+        'Alive': False,
+        'Delay': 0
+    }
 
-    return "Ping:\nHost is dead/timedout :("
+    if delay_s is not None:
+        retval['Alive'] = True
+        retval['Delay'] = delay_s
+
+    return jsonify(retval)
 
 
 def DoDNS(address):
-    retval = 'DNS:\n'
     parts = address.split('.')
 
     # Rough AP parsing
     if len(parts) == 4:
         addr = reversename.from_address(address)
-        return retval + str(resolver.resolve(addr, "PTR")[0])
+        retval = {
+            'Task': 'DNS',
+            'Name': str(resolver.resolve(addr, "PTR")[0])
+        }
+        return jsonify(retval)
+
+    retval = {
+        'Task': 'DNS',
+        'A_Records': [],
+        'MX_Records': [],
+        'CNAME_Records': []
+    }
 
     try:
         a_records = resolver.resolve(address, 'A')
         for a in a_records:
-            retval = retval + "A: {}\n".format(str(a))
+            retval['A_Records'].append(str(a))
     except Exception:
-        retval = retval + "No A records\n"
+        retval['A_Records'] = None
 
     try:
         mx_records = resolver.resolve(address, 'MX')
         for mx in mx_records:
-            retval = retval + "MX: {}\n".format(str(mx))
+            retval['MX_Records'].append(str(mx))
     except Exception:
-        retval = retval + "No MX records\n"
+        retval['MX_Records'] = None
 
     try:
         cname_records = resolver.resolve(address, 'CNAME')
         for cname in cname_records:
-            retval = retval + "CNAME: {}\n".format(str(cname))
+            retval['CNAME_Records'].append(str(cname))
     except Exception:
-        retval+= "No CNAME records\n"
+        retval['CNAME_Records'] = None
 
-    return retval
+    return jsonify(retval)
 
 
 def DoRDAP(address):
+    retval = {
+        'Task': 'RDAP',
+    }
+
     if not whoisit.bootstrap():
-        return "RDAP init failed"
+        retval['Status'] = 'Bootstrap failed'
+        return jsonify(retval)
 
     parts = address.split('.')
 
+    # FIXME: whoisit result doesn't want to be serialized
     if len(parts) == 4:
-        return str(whoisit.ip(address))
+        retval['rst'] = str(whoisit.ip(address))
+    else:
+        retval['rst'] = str(whoisit.domain(address))
 
-    return str(whoisit.domain(address))
+    return jsonify(retval)
 
 
 def DoGeoIP(address):
-    request_url = "https://geolocation-db.com/jsonp/" + address
-    resp = requests.get(request_url)
+    req_url = "https://geolocation-db.com/jsonp/" + address
+    resp = requests.get(req_url)
     result = resp.content.decode()
     result = result.split("(")[1].strip(")")
     result  = json.loads(result)
+    result['Task'] = 'GeoIP'
     return result
 
 
 @app.route('/<addr>/<task>')
 def HandleTask(addr, task):
     task = task.lower()
+
+    # Unsupported task, return Bad Request
     if task not in tasks:
         abort(400)
 
@@ -103,14 +126,10 @@ def HandleTask(addr, task):
 
 def start_worker():
     parser = argparse.ArgumentParser(description='Worker node for IpFinder2')
-    parser.add_argument('--port', type=int, help='server port')
+    parser.add_argument('--port', type=int, help='server port', default="2112")
     args = parser.parse_args()
 
-    port = args.port
-    if port is None:
-        port=2112
-
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=args.port)
 
 
 if __name__ == '__main__':
