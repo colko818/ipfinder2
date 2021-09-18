88b0a7ea4900738d3366b9d8e4b58dbae3df4284
Gaella Collins-Sibley <gaella@collins-sibley.com>
2021-09-18 03:09:44 -0400

    export worker ip/port for manager to read

--- run_local.sh
+++ run_local.sh
@@ -1,11 +1,11 @@
 docker pull gaella818/worker-node:0.1
-docker pull gaella818/manager-node:0.1.1
+docker pull gaella818/manager-node:0.2.1
 
 docker kill worker_node manager_node
 docker rm worker_node manager_node
 
 docker run -d -p 2112:2112 --name worker_node gaella818/worker-node:0.1
-docker run -d -p 5000-5001:5000-5001 --name manager_node gaella818/manager-node:0.2
+docker run -d -p 5000-5001:5000-5001 --name manager_node gaella818/manager-node:0.2.1
 
 docker network create ipfinder
 docker network connect ipfinder worker_node
